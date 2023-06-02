739843905b1b631a2a4f6ba7c6ff2819468f3d83
InnocentZamik <ihlapezulu0@gmail.com>
2023-06-02 16:04:53 +0200

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Home from './pages/home';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return  <Home/>;
+ <ul class="nav justify-content-center">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="#">Active</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link disabled">Disabled</a>
+  </li>
+</ul>
+}
+export default App;
